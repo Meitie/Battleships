0b8951989a025919f22708d1faf498ae16b24933
Meitar S <44778545+Meitie@users.noreply.github.com>
2020-11-30 11:01:16 +0200

    updating the first placed boat

--- computer/comp.py
+++ computer/comp.py
@@ -1,19 +1,43 @@
 import random
 
-computer_board = [[0 for i in range(11)] for j in range(11)]
+def set_up_board():
+    computer_board = [['0' for i in range(11)] for j in range(11)]
 
-for i in range(len(computer_board)):
-    computer_board[0][i] = i
+    for i in range(len(computer_board)):
+        computer_board[0][i] = str(i)
 
-for i in range(1, 11):
-    computer_board[i][0] = chr(ord('A') + i - 1)
+    for i in range(1, 11):
+        computer_board[i][0] = chr(ord('A') + i - 1)
 
-computer_board[0][0] = ""
+    computer_board[0][0] = ""
+    return computer_board
 
-print("---------------------------------------------")
-print("COMPUTERS BOARD")
-for i in computer_board:
-    print(i)
+
+def dictionary_board():
+    dictionary_keys = []
+
+    for i in range(10):
+        for j in range(1, 11):
+            dictionary_keys.append(chr(ord('A') + i) + str(j))
+
+    dictoinary_vals = []
+
+    for i in range(1,11):
+        for j in range(1,11):
+            dictoinary_vals.append((i, j))
+
+    grid_system = {dictionary_keys[i]: dictoinary_vals[i] for i in range(len(dictionary_keys))}
+
+    # print(grid_system)
+    return grid_system
+
+
+
+def print_board(computer_board):
+    print("---------------------------------------------")
+    print("COMPUTERS BOARD")
+    for i in computer_board:
+        print(i)
 
 
 def inside_grid(boat_cords, direction, ship_size):
@@ -26,7 +50,6 @@ def inside_grid(boat_cords, direction, ship_size):
         else:
             return False
     elif direction == 2 or direction == 4:
-        print(f"aa: {letter_cord} bb {chr(ord(letter_cord) + ship_size)}")
         if chr(ord(letter_cord) + ship_size) <= chr(ord('J')):
             return True
         else:
@@ -36,7 +59,7 @@ def inside_grid(boat_cords, direction, ship_size):
 def end_boat(boat_cords, direction, ship_size):
     letter_cord = boat_cords[0]
     num_cord = boat_cords[1]
-    letter_cord = chr(ord(letter_cord) + ship_size)
+    letter_cord = chr(ord(letter_cord) + ship_size - 1)
     boat_cords = letter_cord+num_cord
     print("boat_cords", boat_cords)
     return boat_cords
@@ -45,35 +68,60 @@ def random_direction():
     return random.randint(1,4)
 
 
-def computer_boats():
-    carrier = f"{chr(random.randint(ord('A'), ord('J')))}{random.randint(1,10)}"
-    battleship = f"{chr(random.randint(ord('A'), ord('J')))}{random.randint(1,10)}"
-    cruiser = f"{chr(random.randint(ord('A'), ord('J')))}{random.randint(1,10)}"
-    submarine = f"{chr(random.randint(ord('A'), ord('J')))}{random.randint(1,10)}"
-    destroyer = f"{chr(random.randint(ord('A'), ord('J')))}{random.randint(1,10)}"
+def setup_boats():
+    ships = {
+        "carrier": f"{chr(random.randint(ord('A'), ord('J')))}{random.randint(1,10)}",
+        "battleship": f"{chr(random.randint(ord('A'), ord('J')))}{random.randint(1,10)}",
+        "cruiser": f"{chr(random.randint(ord('A'), ord('J')))}{random.randint(1,10)}",
+        "submarine": f"{chr(random.randint(ord('A'), ord('J')))}{random.randint(1,10)}",
+        "destroyer": f"{chr(random.randint(ord('A'), ord('J')))}{random.randint(1,10)}",
+    }
+    return ships
 
-    print(f"carrier: {carrier}\nbattleships: {battleship}\ncruiser: {cruiser}\nsubmarine: {submarine} \ndestroyer: {destroyer}")
 
-    boats = [carrier, battleship, cruiser, submarine, destroyer]
-    print(boats)
+def print_to_board(board, dict_board, boat_cords):
+    print(boat_cords)
+    if boat_cords[0][1] == boat_cords[1][1]:
+        x = ord(boat_cords[0][0]) - ord("A") + 1
+        y = ord(boat_cords[1][0]) - ord("A") + 1
+        for i in range(x, y + 1):
+            board[i][dict_board[boat_cords[0]][1]] = 's'
+        print_board(board)
+    else:
+        print("letter")
 
-    # for i in range(len(boats)):
-    #     letter.split(chr(),boats)
 
-    # direction = random_direction()
+def place_boats(ships, board, dict_board):
     direction = 2
-    print(random_direction())
+    # direction = random_direction()
+    # print(direction)
     if direction == 1:
-        print(battleship)
-    if direction == 2:
-        print(battleship)
-        if inside_grid(battleship, direction, 5):
-            battleship = (battleship, end_boat(battleship, direction, 4))
-            print(battleship)
-    if direction == 3:
-        print(battleship)
-    if direction == 4:
-        print(battleship)
+        pass
+        # place_boats(ships, board, dict_board)
+    elif direction == 2:
+        print(ships["battleship"])
+        if inside_grid(ships["battleship"], direction, 4):
+            battleship = (ships["battleship"], end_boat(ships["battleship"], direction, 4))
+            print_to_board(board, dict_board, battleship)
+        else:
+            print("reset_it")
+            # place_boats(ships, board, dict_board)
+    elif direction == 3:
+        pass
+        # place_boats(ships, board, dict_board)
+    elif direction == 4:
+        pass
+        # place_boats(ships, board, dict_board)
+    
+
+
+def computer_boats():
+    board = set_up_board()
+    # print_board(board)
+    ships = setup_boats()
+    dict_board = dictionary_board()
+
+    place_boats(ships, board, dict_board)
 
     print(f"The computer has set down its boats, Your Turn! :)")
 
